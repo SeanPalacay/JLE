d3b0660b33622758f6073268a88a092d5d6faf4a
JCBEA <111287289+JCBEA@users.noreply.github.com>
2024-01-22 01:59:27 +0800

    kjgiu

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'QuanbyFormify',
+  appId: 'io.ionic.starter.Formify',
+  appName: 'Formify',
   webDir: 'www',
   server: {
     androidScheme: 'https'
